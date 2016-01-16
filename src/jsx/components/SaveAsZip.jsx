9690186ac1a5e1d204d6c348756c4c6d874341b1
gutchom <yskz.tg@gmail.com>
2016-01-16 22:33:45 +0900

    change filename, add save button module

---
+++ src/jsx/components/SaveAsZip.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class SaveAsZip extends React.Component {
+  handleClick(e) {
+    this.props.onSaveButtonClicked(this.props.list);
+  }
+
+  render() {
+    return(
+      <div className='header__buttons'>
+        <button onClick={this.handleClick.bind(this)}><i className="fa fa-download"></i></button>
+      </div>
+
+    );
+  }
+}
