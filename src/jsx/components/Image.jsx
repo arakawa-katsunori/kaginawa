e7f6fb9fc20d3df63484d789839ad18fe9cba0cf
gutchom <yskz.tg@gmail.com>
2016-07-26 07:56:57 +0900

    display RT and Like button in new window

--- src/jsx/components/Image.jsx
+++ src/jsx/components/Image.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
 export default class Image extends React.Component {
-  handleChange(e) {
-    let value = e.target.value;
-    let checked = e.target.checked;
-    this.props.onCheckboxChanged(value, checked);
+  handleChange(event) {
+    let value = event.target.value
+    let checked = event.target.checked
+    this.props.onCheckboxChanged(value, checked)
   }
 
-  handleClickShowcase(e) {
-    e.preventDefault();
-    this.props.onShowButtonClicked(this.props.tweetId);
+  handleClickShowcase(event) {
+    event.preventDefault()
+    this.props.onShowButtonClicked(this.props.tweetId)
+  }
+
+  openNewWindow(event, url) {
+    event.preventDefault()
+    window.open(url, '_blank', 'width=640, height=480')
   }
 
   render() {
+    const favoriteUrl = 'https://twitter.com/intent/favorite?tweet_id=' + this.props.tweetIdStr
+    const retweetUrl = 'https://twitter.com/intent/retweet?tweet_id=' + this.props.tweetIdStr
     return(
       <div className='image'>
         <input
@@ -22,7 +29,7 @@ export default class Image extends React.Component {
           value={this.props.imageUrl}
           onChange={this.handleChange.bind(this)}
         />
-        <label htmlFor={this.props.tweetId} className='checkIcon'>
+        <label htmlFor={this.props.tweetId} className='check_icon'>
           <i className='fa fa-check' />
         </label>
         <label htmlFor={this.props.tweetId}>
@@ -33,36 +40,34 @@ export default class Image extends React.Component {
             height={this.props.height+'px'}
           />
         </label>
-        <div className='imageMenu'>
+        <div className='image_menu'>
           <a
-            className='imageMenuLinks'
+            className='image_menu__links'
             onClick={this.handleClickShowcase.bind(this)}
           >
             <i className='fa fa-film' />
           </a>
           <a
-            className='imageMenuLinks'
+            className='image_menu__links'
             href={this.props.tweetLink}
             target='_blank'
           >
             <i className='fa fa-twitter' />
           </a>
           <a
-            className='imageMenuLinks'
-            href={'https://twitter.com/intent/favorite?tweet_id=' + this.props.tweetIdStr}
-            target='_blank'
+            className='image_menu__links'
+            onClick={(event, url) => this.openNewWindow(event, favoriteUrl)}
           >
             <i className='fa fa-heart' />
           </a>
           <a
-            className='imageMenuLinks'
-            href={'https://twitter.com/intent/retweet?tweet_id=' + this.props.tweetIdStr}
-            target='_blank'
+            className='image_menu__links'
+            onClick={(event, url) => this.openNewWindow(event, retweetUrl)}
           >
             <i className='fa fa-retweet' />
           </a>
         </div>
       </div>
-    );
+    )
   }
 }
