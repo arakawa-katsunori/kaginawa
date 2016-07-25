69f5f70159f1071562b13060fcf4f1d991509aba
gutchom <yskz.tg@gmail.com>
2016-07-26 07:56:12 +0900

    prepare install redux

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { provider } from 'react-redux'
-import { createStore } from 'redux'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux'
+
+import thunk from 'redux-thunk'
 
 import App from './components/App'
 import Showcase from './components/Showcase'
 
+const store = createStore(
+  combineReducers({
+    routing: routerReducer
+  }),
+  compose(
+    applyMiddleware(routerMiddleware(browserHistory)),
+    applyMiddleware(thunk)
+  )
+)
+
+const history = syncHistoryWithStore(browserHistory, store)
+
 render(
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <Route path='search'>
-        <Route path=':imageId' component={Showcase}/>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path='/' component={App}>
+        <Route path='search'>
+          <Route path=':imageId' component={Showcase}/>
+        </Route>
       </Route>
-    </Route>
-  </Router>
-,document.getElementById('app'));
+    </Router>
+  </Provider>
+, document.getElementById('app'));
